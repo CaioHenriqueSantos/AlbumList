3a5c58ad82381a25818d07d5260dae8e5c609961
Caio Santos <caiohenriquedassantos@gmail.com>
2022-02-02 18:01:06 -0300

    final responsivo

--- src/Spotify/index.js
+++ src/Spotify/index.js
@@ -5,46 +5,46 @@ const clientSecret = '8292a00bc4eb4e38999104e79151eb88';
 const albumId = '2UkBU2McOTulQ1Ki7OwDIw'
 const playlistId = '37i9dQZF1DXd30i5K7UVpZ'
 
-const getToken = async () => {
+const getToken = async() => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded', 
-            'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
         },
-             body: 'grant_type=client_credentials'
+        body: 'grant_type=client_credentials'
     });
     const data = await result.json();
     return data.access_token;
 }
 
-const getAlbum = async (token, albumId )=>{
+const getAlbum = async(token, albumId) => {
 
     const result = await fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks?market=BR&limit=4`, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + token }
 
     });
     const data = await result.json();
     return data.items;
-    
+
 }
-const getPlaylist = async (token, playlistId )=>{
+const getPlaylist = async(token, playlistId) => {
 
     const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?market=BR&limit=4`, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + token }
 
     });
     const data = await result.json();
     return data.items;
-    
+
 }
 
-const getTrack = async(token, trackId)=>{
+const getTrack = async(token, trackId) => {
     const result = await fetch(`https://api.spotify.com/v1/tracks/${trackId}?market=BR`, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + token }
 
     });
     const data = await result.json();
@@ -52,16 +52,16 @@ const getTrack = async(token, trackId)=>{
 }
 
 
-(async()=>{
+(async() => {
 
     const token = await getToken()
     const album = await getAlbum(token, albumId)
     const playlist = await getPlaylist(token, playlistId)
-    const track = await Promise.all(album.map(async ({id}) => {
+    const track = await Promise.all(album.map(async({ id }) => {
         const test = await getTrack(token, id);
         return test
-      }))
+    }))
 
 
-console.log({album: album, playlist: playlist, track: track})
+    console.log({ album: album, playlist: playlist, track: track })
 })()
--- src/apps.js
+++ src/apps.js
@@ -4,13 +4,13 @@ const APIController = (function() {
     const clientSecret = '';
 
     // private methods
-    const _getToken = async () => {
+    const _getToken = async() => {
 
         const result = await fetch('https://acounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
-                'Content-Type' : 'application/x-www-form-urlencoded',
-                'Authorization' : 'Basic' + btoa(clientid + ':' + clientSecret)
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic' + btoa(clientid + ':' + clientSecret)
             },
             body: 'grant_type=client_credentials'
         });
@@ -19,15 +19,15 @@ const APIController = (function() {
         return data.acess_token;
     }
 
-    const _getAlbum = async (token, albumId) => {
+    const _getAlbum = async(token, albumId) => {
 
         const limit = 4;
 
         const result = await fetch(`https://api.spotify.com/v1/albums/2UkBU2McOTulQ1Ki7OwDIw`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
+            headers: { 'Authorization': 'Bearer ' + token }
         });
-        
+
         const data = await result.json();
         return data
 
